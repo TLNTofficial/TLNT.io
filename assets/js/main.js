ce14a82a2b3c52764ef79035077e4c054a705a96
Demiën Drost <demien.drost@iqmedia.nl>
2018-02-22 10:16:49 +0100

    start counting when object in viewport

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,32 @@
-$('.counter').each(function () {
-  var fullNum = $(this).text();
-  // var Decimal = fullNum.split(',')[1];
-  if(typeof Decimal == "string"){
-    Decimal = ","+Decimal;
-  } else {
-    Decimal = "";
-  }
-  $(this).prop('Counter',0).animate({
-    Counter: fullNum
-  }, {
-    duration: 3000,
-    easing: 'swing',
-    step: function (now) {
-      $(this).text(Math.ceil(now));
+$.fn.isInViewport = function() {
+  var elementTop = $(this).offset().top;
+  var elementBottom = elementTop + $(this).outerHeight();
+
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
+
+  return elementBottom > viewportTop && elementTop < viewportBottom;
+};
+
+$(window).on('resize scroll', function() {
+  $('.counter').each(function() {
+    if ($(this).isInViewport()) {
+      var fullNum = $(this).text();
+      var Decimal = fullNum.split(',')[1];
+      if(typeof Decimal == "string"){
+        Decimal = ","+Decimal;
+      } else {
+        Decimal = "";
+      }
+      $(this).prop('Counter',0).animate({
+        Counter: fullNum
+      }, {
+        duration: 3000,
+        easing: 'swing',
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+      });
     }
-  });
-});
+  })
+})
