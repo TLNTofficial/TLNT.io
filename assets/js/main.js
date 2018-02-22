d510af196357ebb675e897ce26ab9950fb7a1998
Demiën Drost <demien.drost@iqmedia.nl>
2018-02-22 10:17:18 +0100

    removed the count*down*

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,25 +8,30 @@ $.fn.isInViewport = function() {
   return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
+var countUp = 0;
+
 $(window).on('resize scroll', function() {
   $('.counter').each(function() {
     if ($(this).isInViewport()) {
-      var fullNum = $(this).text();
-      var Decimal = fullNum.split(',')[1];
-      if(typeof Decimal == "string"){
-        Decimal = ","+Decimal;
-      } else {
-        Decimal = "";
-      }
-      $(this).prop('Counter',0).animate({
-        Counter: fullNum
-      }, {
-        duration: 3000,
-        easing: 'swing',
-        step: function (now) {
-          $(this).text(Math.ceil(now));
+      if(countUp < 3){
+        var fullNum = $(this).text();
+        var Decimal = fullNum.split(',')[1];
+        if(typeof Decimal == "string"){
+          Decimal = ","+Decimal;
+        } else {
+          Decimal = "";
         }
-      });
+        $(this).prop('Counter',0).animate({
+          Counter: fullNum
+        }, {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+            $(this).text(Math.ceil(now));
+          }
+        });
+      }
+      countUp++
     }
   })
 })
